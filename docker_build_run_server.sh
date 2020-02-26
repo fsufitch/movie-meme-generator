937d5b10a8cf2742d285869721c91a85aa3d6b8f
Cameron Copland <ccopland@live.com>
2020-02-25 20:01:46 -0500

    changed docker build file to run in headless mode

--- docker_build_run_server.sh
+++ docker_build_run_server.sh
@@ -3,6 +3,6 @@
 PORT=${PORT:-8080}
 
 docker build -f Dockerfile.server -t moviememes:server . || exit 1
-docker run --rm --name moviememes-server -p $PORT:8080 \
+docker run -d --name moviememes-server -p $PORT:8080 --restart unless-stopped \
     -v $(readlink -f ./resources):/app/resources \
     moviememes:server || exit 1
